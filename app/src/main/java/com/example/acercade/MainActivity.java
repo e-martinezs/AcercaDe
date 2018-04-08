a0a736b38a5847d5eb2c911b6532de392b2380a9
emartinezs <00116316@uca.edu.sv>
2018-04-07 19:15:49 -0600

    Intent para enviar un mensaje

--- app/src/main/java/com/example/acercade/MainActivity.java
+++ app/src/main/java/com/example/acercade/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.acercade;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView githubTextView;
+    TextView facebookTextView;
+    TextView emailTextView;
+    TextView phoneTextView;
+    Button shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,24 @@ public class MainActivity extends AppCompatActivity {
         RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
         roundedBitmapDrawable.setCircular(true);
         imageView.setImageDrawable(roundedBitmapDrawable);
+
+        githubTextView = findViewById(R.id.githubTextView);
+        facebookTextView = findViewById(R.id.facebookTextView);
+        emailTextView = findViewById(R.id.emailTextView);
+        phoneTextView = findViewById(R.id.phoneTextView);
+        shareButton = findViewById(R.id.shareButton);
+
+        String githubText = githubTextView.getText().toString();
+        final String text = "Github: "+githubText;
+
+        shareButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, text);
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent,"Share"));
+            }
+        });
     }
 }
